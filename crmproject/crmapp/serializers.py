fa9de3f9e87df327a11d0477cbcf1b6905daba7c
ubirust <ubirust619@gmail.com>
2024-03-14 17:03:14 +0300

    Доработал serializers.py и listing.html

--- crmproject/crmapp/serializers.py
+++ crmproject/crmapp/serializers.py
@@ -4,6 +4,9 @@ from .models import Listing
 
 
 class ListingSerializer(serializers.ModelSerializer):
+    responsible_name = serializers.CharField(source='responsible.user.first_name', read_only=True)
+
     class Meta:
         model = Listing
-        fields = ('title', 'price', 'address', 'link', 'phone_number', 'responsible', 'call_status', 'created_at', 'agency')
+        fields = (
+        'title', 'price', 'address', 'link', 'phone_number', 'responsible', 'responsible_name', 'call_status', 'created_at', 'agency')
